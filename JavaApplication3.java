c9b105284585151b19e75249ee6bfa1c899eac35
Bhagas Raga Momintan 6706144044 <bagasmomintan@gmail.com>
2016-03-29 09:19:02 +0700

    Added files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int[] bil=new int[100];
+        bil[0]=0;
+        bil[1]=1;       
+        int deret;
+       
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukan jumlah deret : ");
+        String data = input.nextLine();
+        deret = Integer.parseInt(data);
+       
+        for(int i=0;i<deret;i++){
+            System.out.print(bil[i]+" ");
+            bil[i+2]=bil[i]+bil[i+1];
+        }
+    }
+    
+}
