6ba5c21ec060a1d18d7e7ead6a7dd3c052604c2d
Bhagas Raga Momintan 6706144044 <bagasmomintan@gmail.com>
2016-03-29 09:35:48 +0700

    Update JavaApplication3.java

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -23,6 +23,7 @@ public class JavaApplication3 {
         bil[0]=0;
         bil[1]=1;       
         int deret;
+        int jumlah = 0;
        
         Scanner input = new Scanner (System.in);
         System.out.print("Masukan jumlah deret : ");
@@ -30,9 +31,11 @@ public class JavaApplication3 {
         deret = Integer.parseInt(data);
        
         for(int i=0;i<deret;i++){
-            System.out.print(bil[i]+" ");
+            
             bil[i+2]=bil[i]+bil[i+1];
+            jumlah += bil[i];
         }
+        System.out.println(jumlah);
     }
     
 }
